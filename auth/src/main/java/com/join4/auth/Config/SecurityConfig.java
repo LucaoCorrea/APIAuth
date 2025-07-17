81113ba72a4f31c5ee933957818547d8cb0dc50d
LucaoCorrea <lucascorrea1505@gmail.com>
2025-07-17 15:16:19 -0300

    auth resquest

--- auth/src/main/java/com/join4/auth/Config/SecurityConfig.java
+++ auth/src/main/java/com/join4/auth/Config/SecurityConfig.java
@@ -3,29 +3,26 @@ package com.join4.auth.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
-    // Configuração para as permissões de acesso às rotas
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()  // Desabilita CSRF (se necessário)
-            .cors().disable()  // Desabilita CORS
+            .csrf().disable()  
+            .cors().disable()  
             .authorizeRequests()
-                .requestMatchers("/auth/register").permitAll()  // Permite o acesso sem autenticação à rota de registro
-                .requestMatchers("/auth/login").permitAll()     // Permite o acesso sem autenticação à rota de login (se for o caso)
-                .anyRequest().authenticated() // Exige autenticação para todas as outras rotas
+                .requestMatchers("/auth/register").permitAll()  
+                .requestMatchers("/auth/login").permitAll()     
+                .anyRequest().authenticated()
             .and()
             .formLogin()
-                .loginPage("/login")  // Se quiser definir uma página personalizada de login, defina aqui
-                .permitAll()           // Permite que todos acessem a página de login
+                .loginPage("/login")  
+                .permitAll()          
             .and()
-            .httpBasic().disable();  // Desabilita autenticação básica (caso você não queira usar)
+            .httpBasic().disable();  
             
 
         return http.build();
